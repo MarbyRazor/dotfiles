fd96734e22488c6440fc81e1b9ebc6d14d966142
MarbyRazor <marby.razor@web.de>
2022-11-22 13:36:57 +0100

    Removing unused formatters

--- mynvim/.config/nvim/lua/user/lsp/null-ls.lua
+++ mynvim/.config/nvim/lua/user/lsp/null-ls.lua
@@ -16,9 +16,9 @@ null_ls.setup {
       extra_filetypes = { "toml" },
       extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
     },
-    formatting.black.with { extra_args = { "--fast" } },
+    -- formatting.black.with { extra_args = { "--fast" } },
     formatting.stylua,
-    formatting.google_java_format,
+    -- formatting.google_java_format,
     diagnostics.flake8,
   },
 }
