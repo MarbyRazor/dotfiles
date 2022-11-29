31a07057c4aff48f70f72bf5df2e1b48ab6fd0a2
MarbyRazor <marby.razor@web.de>
2022-11-29 12:44:30 +0100

    Blazingly fast on Lazygit and K9s

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -1,40 +1,41 @@
 local status_ok, lsp = pcall(require, "lspconfig")
 if not status_ok then
-  return
+    return
 end
 
-lsp.yamlls.setup {
-  settings = {
-    redhat = { telemetry = { enabled = false } },
-    yaml = {
-      schemas = {
-        ["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.1-standalone-strict/all.json"] = "/*.k8s.yaml",
-      },
+lsp.yamlls.setup({
+    settings = {
+        redhat = { telemetry = { enabled = false } },
+        yaml = {
+            schemas = {
+                ["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json"] = "/*.k8s.yaml",
+                ["http://json.schemastore.org/kustomization"] = "kustomization.yaml",
+            },
+        },
     },
-  }
-}
+})
 
-lsp.terraform_lsp.setup{}
+lsp.terraform_lsp.setup({})
 
-lsp.sumneko_lua.setup {
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
-      },
+lsp.sumneko_lua.setup({
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = "LuaJIT",
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { "vim" },
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
+        },
     },
-  },
-}
+})
